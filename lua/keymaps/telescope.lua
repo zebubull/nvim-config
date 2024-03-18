3a451d290883f42ca3676c051dcfd1ecf8e12f22
zebubull <ldkourtev@gmail.com>
2024-03-18 09:20:30 -0400

    add file grep keybind

--- lua/keymaps/telescope.lua
+++ lua/keymaps/telescope.lua
@@ -1,8 +1,25 @@
+local builtin = require('telescope.builtin')
+local grep_opts = {
+    prompt = 'Grep project',
+}
+
 return {
     {
         "<leader>ff",
         "<cmd>Telescope find_files<CR>",
         desc = "Find file",
+        mode = "n",
+    },
+    {
+        "<leader>fs",
+        function()
+            vim.ui.input(grep_opts, function(text)
+                if text ~= nil then
+                    builtin.grep_string({ search = text })
+                end
+            end)
+        end,
+        desc = "Grep files",
         mode = "n",
     },
 }
